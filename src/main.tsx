b46be976318391836cd9bd72389b0cca2945c810
Seth Zarkovich <sethzarkovich@gmail.com>
2025-08-17 13:06:32 -0400

    Adding Skeleton for React Router

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,13 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import './index.css'
-import App from './App.tsx'
+import { RouterProvider } from "react-router/dom";
+import { router } from './router'
+
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
